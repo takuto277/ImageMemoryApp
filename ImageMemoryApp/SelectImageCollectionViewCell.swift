635b101e6cd247b684fe3be8785ba327ecf46f33
Takuto <Takuto.72.soccer@gmail.com>
2023-05-27 21:39:32 +0900

    add select image screen

---
+++ ImageMemoryApp/SelectImageCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  SelectImageCollectionViewCell.swift
+//  ImageMemoryApp
+//
+//  Created by 小野拓人 on 2023/05/27.
+//
+
+import UIKit
+
+class SelectImageCollectionViewCell: UICollectionViewCell {
+
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ ImageMemoryApp/SelectImageViewController.swift
@@ -0,0 +1,50 @@
+//
+//  SelectImageViewController.swift
+//  ImageMemoryApp
+//
+//  Created by 小野拓人 on 2023/05/27.
+//
+
+import UIKit
+
+final class SelectImageViewController: UIViewController {
+    
+    @IBOutlet weak var SearchTextField: UITextField!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    @IBAction func pushSearchButton(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.collectionView.dataSource = self
+        self.collectionView.delegate = self
+        self.collectionView.register(UINib(nibName: String(describing: SelectImageCollectionViewCell.self), bundle: nil), forCellWithReuseIdentifier: "SelectImageCollectionViewCell")
+    }
+}
+
+extension SelectImageViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SelectImageCollectionViewCell", for: indexPath) as! SelectImageCollectionViewCell
+      //  cell.imageView.image =
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        // 横方向のスペース調整
+        let horizontalSpace:CGFloat = 5
+        
+        //セルのサイズを指定。画面上にセルを3つ表示させたいのであれば、デバイスの横幅を3分割した横幅　- セル間のスペース*2（セル間のスペースが二つあるため）
+        let cellSize:CGFloat = self.view.bounds.width/3 - horizontalSpace*2
+        
+        // 正方形で返すためにwidth,heightを同じにする
+        return CGSize(width: cellSize, height: cellSize)
+    }
+}
+
