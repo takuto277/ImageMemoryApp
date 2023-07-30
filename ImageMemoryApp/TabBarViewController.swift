cc5b192bdf0a0af6ac89fbc086f3b6384b7abf85
Takuto <Takuto.72.soccer@gmail.com>
2023-07-31 02:04:47 +0900

    fix tabBarController

--- ImageMemoryApp/TabBarViewController.swift
+++ ImageMemoryApp/TabBarViewController.swift
@@ -11,14 +11,46 @@ class TabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let viewController1 = ViewControllerFactory.createImageViewController()
-   //     let navigationController1 = UINavigationController(rootViewController: viewController1)
-  //      navigationController1.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 0)
+        // ナビゲーションバー
+        //        navigationBar.tintColor = UIColor.white
+        //        navigationBar.barTintColor = UIColor.red
+        //        navigationBar.isTranslucent = false
+        //        navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.white]
         
-        let viewController2 = ViewControllerFactory.selectImageViewController()
-    //    let navigationController2 = UINavigationController(rootViewController: viewController2)
-   //     navigationController2.tabBarItem = UITabBarItem(tabBarSystemItem: .history, tag: 1)
+        // タブバーコントローラー
+        tabBar.tintColor = UIColor(named: "gold")
+        tabBar.unselectedItemTintColor = UIColor.gray
+        tabBar.barTintColor = UIColor.purple
+        tabBar.isTranslucent = false
         
-        viewControllers = [viewController1, viewController2]
+        // ダークモード時の設定
+        if #available(iOS 13.0, *) {
+            let appearance = UITabBarAppearance()
+            appearance.backgroundColor = UIColor.black // ダークモードの背景色を設定
+            tabBar.standardAppearance = appearance
+            tabBar.scrollEdgeAppearance = appearance
+        }
+        
+        let firstViewController = ViewControllerFactory.homeViewController()
+        let firstTabBerItem = UITabBarItem(tabBarSystemItem: .bookmarks, tag: 1)
+        firstTabBerItem.title = "learn"
+        firstViewController.tabBarItem = firstTabBerItem
+        let firstNavigationController = UINavigationController(rootViewController: firstViewController)
+        
+        
+        let secondViewController = ViewControllerFactory.catalogViewController()
+        secondViewController.tabBarItem = UITabBarItem(tabBarSystemItem: UITabBarItem.SystemItem.search, tag: 2)
+        secondViewController.tabBarItem.title = "search"
+        let secondNavigationController = UINavigationController(rootViewController: secondViewController)
+        
+        
+        let thirdViewController = ViewControllerFactory.createImageViewController()
+        thirdViewController.tabBarItem = UITabBarItem(tabBarSystemItem: UITabBarItem.SystemItem.downloads, tag: 3)
+        thirdViewController.tabBarItem.title = "create"
+        let thirdNavigationController = UINavigationController(rootViewController: thirdViewController)
+        
+        self.viewControllers = [firstNavigationController, secondNavigationController, thirdNavigationController]
+        self.setViewControllers(self.viewControllers, animated:  false)
+        print("🧩\(String(describing: self.viewControllers))")
     }
 }
