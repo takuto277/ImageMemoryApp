06208ba00cbb590cdea7270660ed09ffeb1a4c2d
Takuto <Takuto.72.soccer@gmail.com>
2023-07-31 02:03:41 +0900

    add VC for Factory

--- ImageMemoryApp/Models/DI/ViewControllerFactory.swift
+++ ImageMemoryApp/Models/DI/ViewControllerFactory.swift
@@ -8,6 +8,19 @@
 import UIKit
 
 struct ViewControllerFactory {
+    static func homeViewController() -> UIViewController {
+      //  let presenter = SelectImagePresenter()
+        let vc = HomeViewController()
+  //      vc.modalPresentationStyle = .fullScreen
+        return vc
+    }
+    
+    static func catalogViewController() -> UIViewController {
+    //    let presenter = SelectImagePresenter()
+        let vc = CatalogViewController()
+  //      vc.modalPresentationStyle = .fullScreen
+        return vc
+    }
     static func createImageViewController() -> UIViewController {
         let presenter = CreateImagePresenter()
         let vc = CreateImageViewController(presenter: presenter)
