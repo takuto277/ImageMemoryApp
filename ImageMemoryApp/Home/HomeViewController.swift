bd3ad4c92e84bc6e8ec96acc11ae4c7d7ec844fb
Takuto <Takuto.72.soccer@gmail.com>
2023-07-31 02:02:30 +0900

    add HomeVC

---
+++ ImageMemoryApp/Home/HomeViewController.swift
@@ -0,0 +1,24 @@
+//
+//  HomeViewController.swift
+//  ImageMemoryApp
+//
+//  Created by 小野拓人 on 2023/07/28.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    init() {
+        super.init(nibName: String(describing: HomeViewController.self), bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
