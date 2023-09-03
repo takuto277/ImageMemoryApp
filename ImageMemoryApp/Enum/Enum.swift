f9178f7773278cd931971f83a992ee0f884fcc21
Takuto <Takuto.72.soccer@gmail.com>
2023-09-04 01:42:57 +0900

    add enum of TextView

---
+++ ImageMemoryApp/Enum/Enum.swift
@@ -0,0 +1,17 @@
+//
+//  Enum.swift
+//  ImageMemoryApp
+//
+//  Created by 小野拓人 on 2023/09/04.
+//
+
+import Foundation
+
+enum DetailWordTextViewTag: Int {
+    case englishWord = 1
+    case japanWord = 2
+    case englishSentence = 3
+    case japanSentence = 4
+}
+
+
