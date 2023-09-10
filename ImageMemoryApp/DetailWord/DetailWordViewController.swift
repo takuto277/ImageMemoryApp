f94c50c849b1160242588a444879ca30536496b6
Takuto <Takuto.72.soccer@gmail.com>
2023-09-10 23:39:31 +0900

    just created the DetailWord file

---
+++ ImageMemoryApp/DetailWord/DetailWordViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DetailWordViewController.swift
+//  ImageMemoryApp
+//
+//  Created by 小野拓人 on 2023/09/10.
+//
+
+import UIKit
+
+class DetailWordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
