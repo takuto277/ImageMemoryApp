12cfdb211f211e00518f8301096c0d731a0051e1
Takuto <Takuto.72.soccer@gmail.com>
2023-10-04 01:24:30 +0900

    added English word acquisition logic

--- ImageMemoryApp/Home/HomePresenter.swift
+++ ImageMemoryApp/Home/HomePresenter.swift
@@ -23,6 +23,7 @@ extension HomePresenter: HomeProtocol {
     
     func getLearnWordData() {
         var wordData = [WordData]()
+        var resultWordDatas = [WordData]()
         var fakeImageArray: [String] = []
         do {
             wordData = try self.dataBaseRepository.getLearningWordData()
@@ -30,6 +31,33 @@ extension HomePresenter: HomeProtocol {
             print("英単語の取得に失敗しました。")
         }
         
+        // 学習する英単語の配列を生成
+        let targetNumbers = [["0", "1", "2", "3", "4"], ["5", "6", "7"], ["8", "9", "10"]]
+        
+        targetNumbers.forEach { targetNumber in
+            let filterArray = self.filterStructArray(wordData: wordData, targetNumbers: targetNumber).shuffled()
+            let numberOfElementToGet = filterArray.count >= 5 ? 5 : filterArray.count
+            Array(filterArray.prefix(5)).forEach { wordData in
+                resultWordDatas.append(wordData)
+            }
+        }
+        
+        // 学習する英単語が10個未満の時に補充
+        if resultWordDatas.count < 10 {
+            var wordDatas = wordData
+            wordDatas.shuffle()
+            for wordData in wordDatas {
+                // 登録されている英単語データは登録しない
+                if !resultWordDatas.contains(where: { $0.number == wordData.number }) {
+                    resultWordDatas.append(wordData)
+                }
+                if resultWordDatas.count > 10 {
+                    break
+                }
+            }
+        }
+        
+        // 不正解画像の配列を生成
         for wordData in wordData {
             fakeImageArray.append(wordData.imageURL)
             
@@ -38,8 +66,18 @@ extension HomePresenter: HomeProtocol {
             }
         }
         
-        self.view?.fetchLearnWordData(<#T##wordsData: [WordData]##[WordData]#>, fakeImageArray)
+        self.view?.fetchLearnWordData(resultWordDatas, fakeImageArray)
     }
     
-    
+    private func filterStructArray(wordData: [WordData], targetNumbers: [String]) -> [WordData] {
+        var resultArray = [WordData]()
+        
+        // 該当する優先度の英単語を配列に追加
+        for item in wordData {
+            if targetNumbers.contains(item.priorityNumber) {
+                resultArray.append(item)
+            }
+        }
+        return resultArray
+    }
 }
