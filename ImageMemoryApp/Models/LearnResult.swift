8f99b2f5e65737246c01c24d918c02b09e49ba40
Takuto <Takuto.72.soccer@gmail.com>
2023-10-18 00:46:03 +0900

    add LearnResult model

---
+++ ImageMemoryApp/Models/LearnResult.swift
@@ -0,0 +1,13 @@
+//
+//  LearnResult.swift
+//  ImageMemoryApp
+//
+//  Created by 小野拓人 on 2023/10/18.
+//
+
+import Foundation
+
+struct LearnResult: Codable {
+    let totalCount: String
+    let correctCount: String
+}
