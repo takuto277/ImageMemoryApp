938cc301803b4265375141e8a2e1db85262d46f4
Takuto <Takuto.72.soccer@gmail.com>
2023-10-26 00:45:26 +0900

    add enum proficiency and priorityNumber status

--- ImageMemoryApp/Enum/Enum.swift
+++ ImageMemoryApp/Enum/Enum.swift
@@ -39,3 +39,23 @@ enum UserSelectionFlg {
     case know
     case unknown
 }
+
+enum ProficiencyStatus: Int {
+    case zero = 0
+    case one
+    case two
+}
+
+enum PriorityNumberStatus: Int {
+    case zero = 0
+    case one
+    case two
+    case three
+    case four
+    case five
+    case six
+    case seven
+    case eight
+    case nine
+    case ten
+}
