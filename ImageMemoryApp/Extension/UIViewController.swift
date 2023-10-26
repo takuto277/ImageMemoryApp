2516134379b6f0f3b59e10c172e70cabd7b20929
Takuto <Takuto.72.soccer@gmail.com>
2023-10-27 00:31:06 +0900

    add loading in extension of UIViewController

--- ImageMemoryApp/Extension/UIViewController.swift
+++ ImageMemoryApp/Extension/UIViewController.swift
@@ -10,13 +10,40 @@ import UIKit
 
 extension UIViewController {
     
+    /// キーボード解除機能セット
     func setDismissKeyboard() {
         let tapGR: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
         tapGR.cancelsTouchesInView = false
         self.view.addGestureRecognizer(tapGR)
     }
     
+    /// キーボード解除
     @objc func dismissKeyboard() {
         self.view.endEditing(true)
     }
+    
+    /// ローディング開始
+    func startLoading() {
+        let activityIndicator = UIActivityIndicatorView(style: .large)
+        activityIndicator.center = view.center
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.startAnimating()
+        view.addSubview(activityIndicator)
+    }
+    
+    /// ローディング終了
+    func stopLoading() {
+        for subview in view.subviews {
+            if let activityIndicator = subview as? UIActivityIndicatorView {
+                activityIndicator.stopAnimating()
+                activityIndicator.removeFromSuperview()
+            }
+        }
+    }
 }
+
+
+
+
+
+
